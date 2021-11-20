362ebe01d4c635bb2865c25459d81104c12f4c51
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2021-11-20 20:51:44 +0100

    Fix... maybe

--- main.py
+++ main.py
@@ -294,4 +294,4 @@ def webhook():
 
 if __name__ == '__main__':
     bot.infinity_polling()  # keep running even if there are errors3
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(debug=True, host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
