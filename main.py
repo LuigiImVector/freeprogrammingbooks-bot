9d217fdecf9495334238ecc7e3fd13f55f12fde3
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2021-11-21 13:55:51 +0100

    Typo error

--- main.py
+++ main.py
@@ -292,7 +292,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://freeprogrammingbooks-bot.herokuapp.com//' + TOKEN)
+    bot.set_webhook(url='https://freeprogrammingbooks-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
