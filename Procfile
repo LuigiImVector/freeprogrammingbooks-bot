f81e96c714ea4c35339da3c04f7dc730a169f9f4
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2021-11-21 12:54:41 +0100

    Fixed example

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python3 main.py $PORT
+web: python3 main.py
--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + TOKEN)
+    bot.set_webhook(url='https://freeprogrammingbooks-bot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 
