edd2d1589c6687ac9481264ff73b2f7d85dff974
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2021-11-22 20:01:05 +0100

    Test

--- fpb.py
+++ fpb.py
@@ -8,6 +8,15 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor=conn.cursor()
 
 #cursor.execute('INSERT INTO fpb(id) VALUES (7437246);')
-cursor.execute('DELETE FROM fpb;')
+#cursor.execute('DELETE FROM fpb;')
 
-conn.commit()
+id = 169859925
+
+categoryName = cursor.execute('SELECT choice FROM fpb WHERE id=%s', (id,))
+categoryName = categoryName.fetchall()
+categoryName = ''.join(categoryName[0])
+category = "https://raw.githubusercontent.com/EbookFoundation/free-programming-books/master/" + categoryName
+
+print(category)
+
+#conn.commit()
