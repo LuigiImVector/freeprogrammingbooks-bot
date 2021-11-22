02ab67c2e0856e5492ae20d3c3e920f4201ac2e9
ImVector <59611597+LuigiImVector@users.noreply.github.com>
2021-11-22 19:33:08 +0100

    Test about fpb.py

--- fpb.py
+++ fpb.py
@@ -8,6 +8,23 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 cursor=conn.cursor()
 
 #cursor.execute('INSERT INTO fpb(id) VALUES (7437246);')
-cursor.execute('DELETE FROM fpb')
+#cursor.execute('DELETE FROM fpb;')
 
-conn.commit()
+#conn.commit()
+
+id = 659
+
+username = "LuiginoPaneEVino"
+
+defaultCategory = "books/free-programming-books-langs.md"
+
+cursor.execute('SELECT * FROM fpb WHERE id=?;', (id,))
+
+print("Fetchone: " + cursor.fetchone())
+   
+if cursor.fetchone() is not None:
+    print("Username already saved")
+else:
+    cursor.execute('INSERT INTO fpb(id, username, choice) VALUES (?, ?, ?);', (id, username, defaultCategory,))
+    conn.commit()
+    print("Username saved")
